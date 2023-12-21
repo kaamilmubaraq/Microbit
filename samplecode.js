cd3e778bf69bea406d632642ffc21f2dd969c8d2
Kaamil Mubaraq Hassan <kaamilmilu@gmail.com>
2023-12-21 17:47:31 +0900

    Update samplecode.js

--- samplecode.js
+++ samplecode.js
@@ -1,17 +1,3 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 0) {
-        pins.servoWritePin(AnalogPin.P1, 0)
-        pins.servoWritePin(AnalogPin.P2, 180)
-    } else if (receivedNumber == 1) {
-        pins.servoWritePin(AnalogPin.P1, 180)
-        pins.servoWritePin(AnalogPin.P2, 0)
-    } else if (receivedNumber == 2) {
-        pins.servoWritePin(AnalogPin.P1, 90)
-        pins.servoWritePin(AnalogPin.P2, 90)
-    } else {
-    	
-    }
-})
 input.onButtonPressed(Button.A, function () {
     pins.servoWritePin(AnalogPin.P1, 0)
     pins.servoWritePin(AnalogPin.P2, 180)
